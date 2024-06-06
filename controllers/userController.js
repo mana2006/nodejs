b0af1ef27e99a6db1ec0419b38977fd9703517ae
Hung Nguyen <nguyenhungit.92@gmail.com>
2024-06-07 01:10:12 +0700

    Fix api sign_in & sign_up

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,51 @@
 const firebase = require('../db');
 const User = require('../models/user');
 const firestore = firebase.firestore();
-const {collection, query, where } = require("firebase/firestore");
+const { collection, query, where } = require("firebase/firestore");
 const db = require("../db");
 const functionCommon = require('../utils/common_function');
+const nodeMailer = require('nodemailer');
+const { auth } = require('firebase-admin');
+const jwt = require('jsonwebtoken');
+
+let auth_code_email_signup;
+let auth_code_email_signin;
+
+const sendAuth = async (data, auth_code) => {
+    const transporter = nodeMailer.createTransport({
+        service: 'gmail',
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    })
+    const info = await transporter.sendMail({
+        from: 'no-reply@example.com',
+        to: data.email,
+        subject: 'Verification Code',
+        html: auth_code
+    })
+}
+
+const beforeSignup = async (req, res, next) => {
+    const data = req.body;
+    if (!functionCommon.validateEmail(data.email)) {
+        res.status(404).send('Email invalid, please try agains!');
+    }
+
+    const users = await firestore.collection('users');
+    const userData = await users.where('email', '==', data.email).get();
+    if (userData.empty) {
+        auth_code_email_signup = functionCommon.makeid(10)
+        await sendAuth(data, auth_code_email_signup);
+        res.send('Send auth code successfully');
+    } else {
+        res.status(404).send('Email existed, please choice other email!');
+    }
+}
 
 const userSignup = async (req, res, next) => {
     try {
@@ -17,11 +59,13 @@ const userSignup = async (req, res, next) => {
         const users = await firestore.collection('users');
         const userData = await users.where('email', '==', data.email).get();
         if (userData.empty) {
-            const data_auth = {
-                auth_code: functionCommon.makeid(10),
-                email: data.email
+            if (auth_code_email_signup == data.verify_code) {
+                const data_auth = {
+                    auth_code: auth_code_email_signup,
+                    email: data.email
+                }
+                await firestore.collection('users').doc().set(data_auth);
             }
-            await firestore.collection('users').doc().set(data_auth);
         } else {
             res.status(404).send('Email existed, please choice other email!');
         }
@@ -32,23 +76,53 @@ const userSignup = async (req, res, next) => {
     }
 }
 
+const beforeSignin = async (req, res, next) => {
+    const data = req.body;
+    if (!functionCommon.validateEmail(data.email)) {
+        res.status(404).send('Email invalid, please try agains!');
+    }
+
+    const users = await firestore.collection('users');
+    const userData = await users.where('email', '==', data.email).get();
+    if (!userData.empty) {
+        let userInfo = {};
+            userData.forEach(doc => {
+                userInfo = {
+                    'email': doc.data().email,
+                    'auth_code': doc.data().auth_code
+                }
+            });
+            auth_code_email_signin = userInfo.auth_code;
+            await sendAuth(userInfo, userInfo.auth_code);
+        res.send('Send auth code successfully');
+    } else {
+        res.status(404).send('Email existed, please choice other email!');
+    }
+}
 
 const userSignin = async (req, res, next) => {
     try {
         const data = req.body;
-
-        const users = await firestore.collection('users');
-        const userData = await users.where('email', '==', data.email).get();
-        if (!userData.empty) {
-            const data_auth = {
-                auth_code: functionCommon.makeid(10),
-                email: data.email
-            }
-            await firestore.collection('auth_users').doc().set(data_auth);
-        } else {
-            res.status(404).send('No user record found!');
+        if (!functionCommon.validateEmail(data.email)) {
+            res.status(404).send('Email invalid, please try agains!');
         }
-        res.send('Record saved successfully');
+        const users = await firestore.collection('users');
+        const userData = await users.where('email', '==', data.email).where('auth_code', '==', auth_code_email_signin).get();
+
+        if (!userData.empty) {
+            let userInfo = {};
+            userData.forEach(doc => {
+                userInfo = {
+                    'email': doc.data().email,
+                    'auth_code': doc.data().auth_code
+                }
+            });
+            const token = jwt.sign({userInfo} , process.env.JWT_SECRET_KEY);
+            res.send({accessToken: token});
+        } else {
+            res.status(404).send('Auth Code wrong!');
+        }
+        
     } catch (error) {
         res.status(400).send(error.message);
     }
@@ -68,4 +142,6 @@ const userSignin = async (req, res, next) => {
 module.exports = {
     userSignup,
     userSignin,
+    beforeSignup,
+    beforeSignin
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const { userSignup, userSignin } = require('../controllers/userController');
+const { userSignup, userSignin, beforeSignup, beforeSignin } = require('../controllers/userController');
 
 const router = express.Router();
 
+router.post('/auth/before_signup', beforeSignup);
 
 router.post('/auth/signup', userSignup);
 
 router.post('/auth/signin', userSignin);
+
+router.post('/auth/before_signin', beforeSignin);
 
 module.exports = {
     routes: router
